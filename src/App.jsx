e796471fb48392ebedd738156a8eb15fdb383081
k417hussain <k417hussain@gmail.com>
2024-05-14 21:42:50 +0530

    some basic setup (layout)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
-
+import MetronomeSoundBtn from './components/MetronomeSoundBtn'
 function App() {
 
   return (
     <>
-    hey there
+      <header>
+        <img src='../public/BerryMetronomeLogo.jpeg' id='logo'></img>
+        Berry Metronome
+        </header>
+      <div id='metronomeBtnBox'>
+        <MetronomeSoundBtn/>
+        <MetronomeSoundBtn/>
+        <MetronomeSoundBtn/>
+      </div>
+      <div id='metronomeToolBox'>Metronome tool box</div>
     </>
   )
 }
---
+++ src/components/MetronomeSoundBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './MetronomeSoundBtn.css'
+const MetronomeSoundBtn = () => {
+  return (
+    <div id='MetronomeSoundBtn'></div>
+  )
+}
+
+export default MetronomeSoundBtn
