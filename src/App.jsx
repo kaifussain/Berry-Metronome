0543ca19ccf7ac560485105372f4a3cb2680497a
k417hussain <k417hussain@gmail.com>
2024-05-16 16:52:29 +0530

    built the tempo slider UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { useState } from "react";
+import {useState, useEffect } from "react";
 import "./App.css";
 import MetronomeSoundBtn from "./components/MetronomeSoundBtn";
 function App() {
   const [beatspb_v, setBeatpb_v] = useState(3)
   const [currentTempo_v, setCurrentTempo_v] = useState(20)
+  const [currentTempoName_v, setCurrentTempoName_v] = useState("Larghissimo")
 
   function handleBeatsPbar_f(e){
     setBeatpb_v(parseInt(e.target.value, 10))
   }
+ 
+  useEffect(() => {
+    function updateTempoName_f() {
+      if (currentTempo_v < 20) setCurrentTempoName_v("Larghissimo");
+      else if(currentTempo_v<40) setCurrentTempoName_v("Grave");
+      else if(currentTempo_v>=40 && currentTempo_v<=60) setCurrentTempoName_v("Lento/Largo");
+      else if(currentTempo_v>60 && currentTempo_v<66) setCurrentTempoName_v("");
+      else if(currentTempo_v>=66 && currentTempo_v<=76) setCurrentTempoName_v("Adagio");
+      else if(currentTempo_v>76 && currentTempo_v<=80) setCurrentTempoName_v("Adagietto");
+      else if(currentTempo_v>80 && currentTempo_v<=108) setCurrentTempoName_v("Andante");
+      else if(currentTempo_v>108 && currentTempo_v<=120) setCurrentTempoName_v("Moderato");
+      else if(currentTempo_v>120 && currentTempo_v<=168) setCurrentTempoName_v("Allegro");
+      else if(currentTempo_v>168 && currentTempo_v<=176) setCurrentTempoName_v("Vivace");
+      else if(currentTempo_v>176 && currentTempo_v<=200) setCurrentTempoName_v("Presto");
+      else if(currentTempo_v>200) setCurrentTempoName_v("Prestissimo")
+    }
 
+    updateTempoName_f();
+  }, [currentTempo_v]);
 
   return (
     <>
       <div id="forTestingOnly">
-        beatspb_v={beatspb_v} <br/>
-        currentTempo_v={currentTempo_v}
+        {/* beatspb_v={beatspb_v} <br/> */}
+        {/* currentTempo_v={currentTempo_v} <br/> */}
+        {/* {tempoSP_ref.} */}
+        {currentTempoName_v}
+      
       </div>
+
+
       <header>
         <img src="../public/BerryMetronomeLogo.jpeg" id="logo"></img>
         Berry Metronome
@@ -43,9 +67,22 @@ function App() {
       </div>
 
 
-      <div id="tempoSlider">
-        <div id="tempoSliderPointer" draggable={true}></div>
-      </div>
+     <div id="tempoSliderBox">
+        <div id="tempoRangeBox">
+        <div id="currentTempoName">{currentTempoName_v}</div>
+          <div className="eachTempoRange verySlow" 
+          onClick={()=>setCurrentTempo_v(30)}></div>
+          <div className="eachTempoRange slow"
+          onClick={()=>setCurrentTempo_v(60)}></div>
+          <div className="eachTempoRange moderate"
+          onClick={()=>setCurrentTempo_v(100)}></div>
+          <div className="eachTempoRange fast"
+          onClick={()=>setCurrentTempo_v(160)}></div>
+          <div className="eachTempoRange veryFast"
+          onClick={()=>setCurrentTempo_v(200)}></div>
+        </div>
+        <input id="tempRInp" type="range" min={0} max={300} value={currentTempo_v} onChange={e=>setCurrentTempo_v(e.target.value)}></input>
+     </div>
 
 
       <div id="randomBox">
