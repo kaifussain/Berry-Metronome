080e8c56810e7863b4403b4ba6991fb99a901b08
k417hussain <k417hussain@gmail.com>
2024-05-18 23:23:47 +0530

    Built the main functionality for metronome and audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import MetronomeSoundBtn from "./components/MetronomeSoundBtn";
 function App() {
-  const [beatspb_v, setBeatpb_v] = useState(3)
-  const [currentTempo_v, setCurrentTempo_v] = useState(20)
-  const [currentTempoName_v, setCurrentTempoName_v] = useState("Larghissimo")
+  const [playing, setPlaying] = useState(false);
+  const [beatspb_v, setBeatpb_v] = useState(2);
+  const [currentTempo_v, setCurrentTempo_v] = useState(20);
+  const [currentTempoName_v, setCurrentTempoName_v] = useState("Larghissimo");
+  const [currentBeat, setCurrentBeat] = useState(0);
 
-  function handleBeatsPbar_f(e){
-    setBeatpb_v(parseInt(e.target.value, 10))
+  function handleBeatsPbar_f(e) {
+    if (playing) {
+      setPlaying(false);
+    }
+    setBeatpb_v(parseInt(e.target.value, 10));
   }
- 
+
   useEffect(() => {
     function updateTempoName_f() {
       if (currentTempo_v < 20) setCurrentTempoName_v("Larghissimo");
-      else if(currentTempo_v<40) setCurrentTempoName_v("Grave");
-      else if(currentTempo_v>=40 && currentTempo_v<=60) setCurrentTempoName_v("Lento/Largo");
-      else if(currentTempo_v>60 && currentTempo_v<66) setCurrentTempoName_v("");
-      else if(currentTempo_v>=66 && currentTempo_v<=76) setCurrentTempoName_v("Adagio");
-      else if(currentTempo_v>76 && currentTempo_v<=80) setCurrentTempoName_v("Adagietto");
-      else if(currentTempo_v>80 && currentTempo_v<=108) setCurrentTempoName_v("Andante");
-      else if(currentTempo_v>108 && currentTempo_v<=120) setCurrentTempoName_v("Moderato");
-      else if(currentTempo_v>120 && currentTempo_v<=168) setCurrentTempoName_v("Allegro");
-      else if(currentTempo_v>168 && currentTempo_v<=176) setCurrentTempoName_v("Vivace");
-      else if(currentTempo_v>176 && currentTempo_v<=200) setCurrentTempoName_v("Presto");
-      else if(currentTempo_v>200) setCurrentTempoName_v("Prestissimo")
+      else if (currentTempo_v < 40) setCurrentTempoName_v("Grave");
+      else if (currentTempo_v >= 40 && currentTempo_v <= 60)
+        setCurrentTempoName_v("Lento/Largo");
+      else if (currentTempo_v > 60 && currentTempo_v < 66)
+        setCurrentTempoName_v("");
+      else if (currentTempo_v >= 66 && currentTempo_v <= 76)
+        setCurrentTempoName_v("Adagio");
+      else if (currentTempo_v > 76 && currentTempo_v <= 80)
+        setCurrentTempoName_v("Adagietto");
+      else if (currentTempo_v > 80 && currentTempo_v <= 108)
+        setCurrentTempoName_v("Andante");
+      else if (currentTempo_v > 108 && currentTempo_v <= 120)
+        setCurrentTempoName_v("Moderato");
+      else if (currentTempo_v > 120 && currentTempo_v <= 168)
+        setCurrentTempoName_v("Allegro");
+      else if (currentTempo_v > 168 && currentTempo_v <= 176)
+        setCurrentTempoName_v("Vivace");
+      else if (currentTempo_v > 176 && currentTempo_v <= 200)
+        setCurrentTempoName_v("Presto");
+      else if (currentTempo_v > 200) setCurrentTempoName_v("Prestissimo");
     }
 
     updateTempoName_f();
+    document.documentElement.style.setProperty("--tempo", currentTempo_v);
   }, [currentTempo_v]);
 
+  useEffect(() => {
+    let intervalId;
+
+    if (playing) {
+      intervalId = setInterval(() => {
+        setCurrentBeat((prevBeat) => (prevBeat + 1) % beatspb_v);
+      }, (60 / currentTempo_v) * 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [playing, currentTempo_v, beatspb_v]);
+
+ 
   return (
     <>
       <div id="forTestingOnly">
@@ -36,65 +64,106 @@ function App() {
         {/* currentTempo_v={currentTempo_v} <br/> */}
         {/* {tempoSP_ref.} */}
         {/* {currentTempoName_v} */}
-      
       </div>
-
-
-
+    <div id="soundSelect">
+      Sound: 
+      <select >
+        <option value={'a'}>a</option>
+        <option value={'b'}>b</option>
+      </select>
+    </div>
       <header>
         <img src="../public/BerryMetronomeLogo.jpeg" id="logo"></img>
         Berry Metronome
       </header>
       <div id="metronomeBtnBox">
-        {
-          [...Array(beatspb_v)].map((_,i)=>(
-            <MetronomeSoundBtn key={i}/>
-          ))
-        }
+        {[...Array(beatspb_v)].map((_, i) => (
+          <MetronomeSoundBtn
+            key={i}
+            play={playing && i === currentBeat}
+            playing={playing}
+          />
+        ))}
       </div>
 
-
-      <div id="metronmePointer">
+      <div id="metronmePointer"
+        style={{
+          animation: playing
+            ? `metronomePointerAni ${120 / currentTempo_v}s infinite ease-in-out`
+            : "none",
+        }}
+      >
         <div></div>
       </div>
 
-
       <div id="metronomeToolBox">
-        <button className="tempoCtrlBtn" onClick={()=>setCurrentTempo_v(x=>Math.max(0,x-20))}>&lt;&lt;</button>
-        <button className="tempoCtrlBtn" onClick={()=>setCurrentTempo_v(x=>Math.max(0,x-5))}>&lt;</button>
+        <button
+          className="tempoCtrlBtn"
+          onClick={() => setCurrentTempo_v((x) => Math.max(1, x - 5))}
+        >
+          &lt;&lt;
+        </button>
+        <button
+          className="tempoCtrlBtn"
+          onClick={() => setCurrentTempo_v((x) => Math.max(1, x - 1))}
+        >
+          &lt;
+        </button>
         <div id="currentTempoBox">{currentTempo_v}</div>
-        <button className="tempoCtrlBtn" onClick={()=>setCurrentTempo_v(x=>Math.min(x+5,300))}>&gt;</button>
-        <button className="tempoCtrlBtn" onClick={()=>setCurrentTempo_v(x=>Math.min(x+20,300))}>&gt;&gt;</button>
+        <button
+          className="tempoCtrlBtn"
+          onClick={() => setCurrentTempo_v((x) => Math.min(x + 1, 300))}
+        >
+          &gt;
+        </button>
+        <button
+          className="tempoCtrlBtn"
+          onClick={() => setCurrentTempo_v((x) => Math.min(x + 5, 300))}
+        >
+          &gt;&gt;
+        </button>
       </div>
 
-
-     <div id="tempoSliderBox">
+      <div id="tempoSliderBox">
         <div id="tempoRangeBox">
-        <div id="currentTempoName">{currentTempoName_v}</div>
-          <div className="eachTempoRange verySlow" 
-          onClick={()=>setCurrentTempo_v(30)}></div>
-          <div className="eachTempoRange slow"
-          onClick={()=>setCurrentTempo_v(60)}></div>
-          <div className="eachTempoRange moderate"
-          onClick={()=>setCurrentTempo_v(100)}></div>
-          <div className="eachTempoRange fast"
-          onClick={()=>setCurrentTempo_v(160)}></div>
-          <div className="eachTempoRange veryFast"
-          onClick={()=>setCurrentTempo_v(200)}></div>
+          <div id="currentTempoName">{currentTempoName_v}</div>
+          <div
+            className="eachTempoRange verySlow"
+            onClick={() => setCurrentTempo_v(30)}
+          ></div>
+          <div
+            className="eachTempoRange slow"
+            onClick={() => setCurrentTempo_v(60)}
+          ></div>
+          <div
+            className="eachTempoRange moderate"
+            onClick={() => setCurrentTempo_v(100)}
+          ></div>
+          <div
+            className="eachTempoRange fast"
+            onClick={() => setCurrentTempo_v(160)}
+          ></div>
+          <div
+            className="eachTempoRange veryFast"
+            onClick={() => setCurrentTempo_v(200)}
+          ></div>
         </div>
-        <input id="tempRInp" type="range" min={0} max={300} value={currentTempo_v} onChange={e=>setCurrentTempo_v(e.target.value)}></input>
-     </div>
-
-
-      <div id="randomBox">
-        randomBox
+        <input
+          id="tempRInp"
+          type="range"
+          min={1}
+          max={300}
+          value={currentTempo_v}
+          onChange={(e) => setCurrentTempo_v(Number(e.target.value))}
+        ></input>
       </div>
 
+      <div id="randomBox">randomBox</div>
 
       <div id="playCtrlBox">
         <div id="beatsPbarBtn">
           Beats/bar
-          <select value={beatspb_v} onChange={(e)=>handleBeatsPbar_f(e)}>
+          <select value={beatspb_v} onChange={(e) => handleBeatsPbar_f(e)}>
             <option value={1}>1</option>
             <option value={2}>2</option>
             <option value={3}>3</option>
@@ -103,9 +172,13 @@ function App() {
             <option value={6}>6</option>
             <option value={7}>7</option>
             <option value={8}>8</option>
+            <option value={9}>9</option>
+            <option value={10}>10</option>
           </select>
         </div>
-        <div id="playBtn">Play</div>
+        <div id="playBtn" onClick={() => setPlaying(!playing)}>
+          {playing ? "Stop" : "Play"}
+        </div>
         <div id="timerBtn">Timer</div>
       </div>
     </>
--- src/components/MetronomeSoundBtn.jsx
+++ src/components/MetronomeSoundBtn.jsx
@@ -1,10 +1,70 @@
-import React, { useState } from 'react'
-import './MetronomeSoundBtn.css'
-const MetronomeSoundBtn = () => {
-  const [mode,setMode]=useState(1)
-  return (
-    <div id='MetronomeSoundBtn' className={mode===0?'mode0':(mode===1?'mode1':'mode2')} onClick={()=>setMode(m=>(m+1)%3)}></div>
-  )
-}
+import React, { useEffect, useRef, useState } from "react";
+import "./MetronomeSoundBtn.css";
+const MetronomeSoundBtn = ({play,playing}) => {
+  const [mode, setMode] = useState(1);
+  const beat1 = useRef(null);
+  const beat2 = useRef(null);
+  const beat3 = useRef(null);
 
-export default MetronomeSoundBtn
+  useEffect(() => {
+    if (play) {
+      if (mode !== 0) {
+
+        if (mode === 1) {
+          if (beat1.current) {
+            beat1.current.currentTime = 0;
+            beat1.current.play();
+          }
+        } 
+        
+        else if(mode===2){
+          if (beat2.current) {
+            beat2.current.currentTime = 0;
+            beat2.current.play();
+          }
+        }
+
+        else{
+          if (beat3.current) {
+          beat3.current.currentTime = 0;
+          beat3.current.play();
+        }
+      }
+    }
+    }}, [play, mode]);
+
+
+  return (
+    <div
+      id="MetronomeSoundBtn"
+      className={`${
+        mode === 0 ? "mode0" : mode === 1 ? "mode1" : mode === 3 ? "mode3" : "mode2"
+      } ${play ? "btnPlaying" : ""}`}
+      
+      onClick={() => {
+        let m = (mode + 1) % 4;
+        setMode(m);
+        if(!playing){
+          if(m===1){
+            beat1.current.currentTime = 0;
+            beat1.current.play();
+          }
+          else if(m===2){
+            beat2.current.currentTime = 0;
+            beat2.current.play();
+          }
+          else if(m===3){
+            beat3.current.currentTime = 0;
+            beat3.current.play();
+          }
+        }
+      }}
+    >
+      <audio ref={beat1} src="src\assets\sound_a\a1.mp3"></audio>
+      <audio ref={beat2} src="src\assets\sound_a\a2.mp3"></audio>
+      <audio ref={beat3} src="src\assets\sound_a\a3.wav"></audio>
+    </div>
+  );
+};
+
+export default MetronomeSoundBtn;
