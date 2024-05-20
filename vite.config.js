e9b831ae5385872f93553d975bbe51b8de576898
k417hussain <k417hussain@gmail.com>
2024-05-20 23:44:27 +0530

    setup to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/berry-metronome/",
   plugins: [react()],
-  server: {
-    host: '0.0.0.0'
-  }
 })
